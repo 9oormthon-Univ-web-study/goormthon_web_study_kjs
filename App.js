14c0bcf8e8bffd0ddf97c47a585cf9ee020477c9
kojesung <js95112345@gmail.com>
2024-08-17 00:13:41 +0900

    클래스 기반 컴포넌트로 틱택토 게임 구현(state와 props전달)

---
+++ App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Board from './components/Board';
+
+export default function App() {
+    return (
+        <div className="game">
+            <div className="game-board">
+                <Board />
+            </div>
+            <div className="game-info">game-info</div>
+        </div>
+    );
+}
