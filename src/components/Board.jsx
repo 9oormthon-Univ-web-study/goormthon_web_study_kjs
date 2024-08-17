48283ea21cd2131a0d879450af015b19e96495c7
kojesung <js95112345@gmail.com>
2024-08-17 16:13:26 +0900

    refactor : 클래스형 컴포넌트 -> 함수형 컴포넌트

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -1,43 +1,36 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import Square from './Square';
 import './Board.css';
 
-export default class Board extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            squares: Array(9).fill(null), //this.state = ,,,,,,,,인 상태(null로 가득참)
-        };
-    }
+const Board = () => {
+    const [squares, setSquare] = useState(Array(9).fill(null));
 
-    handleClick(i) {
-        const squares = this.state.squares.slice(); //null로 가득찬 squares배열을 얕은 복사함
-        squares[i] = 'X'; //얕은 복사본의 배열의 인자값 인덱스를 X로 변경
-        this.setState({
-            squares: squares,
-        });
-    }
+    const handleClick = (i) => {
+        const newSquares = squares.slice(); //null로 가득찬 squares배열을 얕은 복사함
+        newSquares[i] = 'X'; //얕은 복사본의 배열의 인자값 인덱스를 X로 변경
+        setSquare(newSquares);
+    };
 
-    render() {
-        return (
-            <div>
-                <div className="status">다음에 둘 사람은 X냐 O냐를 표시해줄 곳</div>
-                <div className="board-row">
-                    <Square value={this.state.squares[0]} onClick={() => this.handleClick(0)} />
-                    <Square value={this.state.squares[1]} onClick={() => this.handleClick(1)} />
-                    <Square value={this.state.squares[2]} onClick={() => this.handleClick(2)} />
-                </div>
-                <div className="board-row">
-                    <Square value={this.state.squares[3]} onClick={() => this.handleClick(3)} />
-                    <Square value={this.state.squares[4]} onClick={() => this.handleClick(4)} />
-                    <Square value={this.state.squares[5]} onClick={() => this.handleClick(5)} />
-                </div>
-                <div className="board-row">
-                    <Square value={this.state.squares[6]} onClick={() => this.handleClick(6)} />
-                    <Square value={this.state.squares[7]} onClick={() => this.handleClick(7)} />
-                    <Square value={this.state.squares[8]} onClick={() => this.handleClick(8)} />
-                </div>
+    return (
+        <div>
+            <div className="status">다음에 둘 사람은 X냐 O냐를 표시해줄 곳</div>
+            <div className="board-row">
+                <Square value={squares[0]} onClick={() => handleClick(0)} />
+                <Square value={squares[1]} onClick={() => handleClick(1)} />
+                <Square value={squares[2]} onClick={() => handleClick(2)} />
             </div>
-        );
-    }
-}
+            <div className="board-row">
+                <Square value={squares[3]} onClick={() => handleClick(3)} />
+                <Square value={squares[4]} onClick={() => handleClick(4)} />
+                <Square value={squares[5]} onClick={() => handleClick(5)} />
+            </div>
+            <div className="board-row">
+                <Square value={squares[6]} onClick={() => handleClick(6)} />
+                <Square value={squares[7]} onClick={() => handleClick(7)} />
+                <Square value={squares[8]} onClick={() => handleClick(8)} />
+            </div>
+        </div>
+    );
+};
+
+export default Board;
--- src/components/Square.jsx
+++ src/components/Square.jsx
@@ -1,16 +1,12 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './Square.css';
 
-export default class Square extends Component {
-    //공유 state를 상위 컴포넌트인 Board에서 관리하므로 전에 내용 지움
-
-    render() {
-        //함수형 컴포넌트가 아닌 인자를 받지 않은 class형 컴포넌트에서는 this.props로 받은 props에 접근할 수 있음
-        //어떻게 접근할 수 있냐?? React 내부에서 임의로 `Square.props = props;`라고 선언을 해주었음
-        return (
-            <button onClick={() => this.props.onClick()} className="square">
-                {this.props.value}
-            </button>
-        );
-    }
-}
+const Square = ({ value, onClick }) => {
+    return (
+        <button onClick={onClick} className="square">
+            {value}
+        </button>
+    );
+};
+//`onClick = {onClick}` == `onClick = {()=>{onClick()}}`같은 뜻
+export default Square;
