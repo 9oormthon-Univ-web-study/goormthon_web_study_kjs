a8051d6e18c24129666340522ac25d4c73bd5727
kojesung <js95112345@gmail.com>
2024-08-18 13:31:10 +0900

    refactor : 기록 보관을 위해 상태관리 위치 App.js로 이동

--- src/App.js
+++ src/App.js
@@ -1,13 +1,61 @@
-import React from 'react';
+// @ts-nocheck
+import React, { useState } from 'react';
 import Board from './components/Board';
 
 export default function App() {
+    const [history, setHistory] = useState([{ squares: Array(9).fill(null) }]);
+    const [xIsNext, setXIsNext] = useState(true);
+
+    const calculateWinner = (squares) => {
+        const lines = [
+            [0, 1, 2],
+            [3, 4, 5],
+            [6, 7, 8],
+            [0, 3, 6],
+            [1, 4, 7],
+            [2, 5, 8],
+            [0, 4, 8],
+            [2, 4, 6],
+        ]; // 승자가 나오는 모든 경우의 수를 배열로 나타냄
+
+        for (let index = 0; index < lines.length; index++) {
+            const [a, b, c] = lines[index];
+            if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+                return squares[a];
+            }
+        }
+        return null;
+    };
+
+    const current = history[history.length - 1];
+    const winner = calculateWinner(current.squares);
+
+    let gameStatus;
+
+    if (winner) {
+        gameStatus = 'Winner: ' + winner;
+    } else {
+        gameStatus = `다음에 둘 사람은 ${xIsNext ? 'X' : 'O'}`;
+    }
+
+    const handleClick = (i) => {
+        const newSquares = current.squares.slice();
+        if (calculateWinner(newSquares) || newSquares[i]) {
+            return;
+        }
+
+        newSquares[i] = xIsNext ? 'X' : 'O';
+        setHistory([...history, { squares: newSquares }]);
+        setXIsNext((prev) => !prev);
+    };
+
     return (
         <div className="game">
             <div className="game-board">
-                <Board />
+                <Board squares={current.squares} onClick={(i) => handleClick(i)} />
+                {/* handleClick함수가 인자를 필요로하기 때문에 이 형식으로 전달 */}
             </div>
-            <div className="game-info">game-info</div>
+            <div className="game-info">{gameStatus}</div>
         </div>
     );
 }
--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -2,70 +2,34 @@ import React, { useState } from 'react';
 import Square from './Square';
 import './Board.css';
 
-const Board = () => {
-    const [squares, setSquare] = useState(Array(9).fill(null));
-    const [xIsNext, setXIsNext] = useState(true);
-
-    const handleClick = (i) => {
-        const newSquares = squares.slice(); //null로 가득찬 squares배열을 얕은 복사함
-        if (calculateWinnder(squares) || newSquares[i]) {
-            return;
-        }
-        newSquares[i] = xIsNext ? 'X' : 'O';
-        setSquare(newSquares);
-        setXIsNext((prev) => !prev);
-        //함수형 업데이트라고 부름, setState는 비동기적으로 발생하기 때문에 이전의 값을 전달받지 못함,
-        //setState함수는 현재 상태를 인수로 받는 함수를 인수로 취하고 새로운 상태를 return하기 때문에 위와 같은 형태로 작성이 가능함
+const Board = ({ squares, onClick }) => {
+    const renderSquare = (i) => {
+        return (
+            <Square
+                value={squares[i]}
+                onClick={() => {
+                    onClick(i);
+                }}
+            />
+        );
     };
-
-    const calculateWinnder = (squares) => {
-        const lines = [
-            [0, 1, 2],
-            [3, 4, 5],
-            [6, 7, 8],
-            [0, 3, 6],
-            [1, 4, 7],
-            [2, 5, 8],
-            [0, 4, 8],
-            [2, 4, 6],
-        ]; //승자가 나오는 모든 경우의 수를 배열로 나타냄
-
-        for (let index = 0; index < lines.length; index++) {
-            const [a, b, c] = lines[index];
-            if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-                return squares[a];
-            }
-        }
-        return null;
-    };
-
-    const winner = calculateWinnder(squares);
-
-    let gameStatus;
-
-    if (winner) {
-        gameStatus = 'Winner' + winner;
-    } else {
-        gameStatus = `다음에 둘 사람은 ${xIsNext ? 'X' : 'O'}`;
-    }
 
     return (
         <div>
-            <div className="status">{gameStatus}</div>
             <div className="board-row">
-                <Square value={squares[0]} onClick={() => handleClick(0)} />
-                <Square value={squares[1]} onClick={() => handleClick(1)} />
-                <Square value={squares[2]} onClick={() => handleClick(2)} />
+                {renderSquare(0)}
+                {renderSquare(1)}
+                {renderSquare(2)}
             </div>
             <div className="board-row">
-                <Square value={squares[3]} onClick={() => handleClick(3)} />
-                <Square value={squares[4]} onClick={() => handleClick(4)} />
-                <Square value={squares[5]} onClick={() => handleClick(5)} />
+                {renderSquare(3)}
+                {renderSquare(4)}
+                {renderSquare(5)}
             </div>
             <div className="board-row">
-                <Square value={squares[6]} onClick={() => handleClick(6)} />
-                <Square value={squares[7]} onClick={() => handleClick(7)} />
-                <Square value={squares[8]} onClick={() => handleClick(8)} />
+                {renderSquare(6)}
+                {renderSquare(7)}
+                {renderSquare(8)}
             </div>
         </div>
     );
